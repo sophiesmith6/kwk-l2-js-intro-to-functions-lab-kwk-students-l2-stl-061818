e4d6c727afb35b4040fb4fb9ade6ea309b51180a
sophiesmith6 <18smiths@corjesu.org>
2018-06-21 14:55:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+  function getUKTime() {
+    let UKTime = new Date().getHours() + 5
+    return UKTime
+} 
+
+  function discountTicket(originalPrice) {
+    let actualPrice = originalPrice * 0.8;
+    return actualPrice
+  }
+  
+  function tweetReply(fansName) {
+    let greeting = `Hey ${fansName}, thanks so much for the support! I really appreciate it!`
+    return greeting
+  }
